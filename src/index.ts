7cc0ef76f346bd4995247d9c632b337dbf10c7bb
Clark Tozer <clarkwtozer@gmail.com>
2020-01-16 21:18:28 +1100

    first run untested

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,58 @@
-import { takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery } from "redux-saga/effects";
 
-function* fetchWorker() {
-    console.log("fetchWorker");
+export interface FetchPayload<T, E> {
+    request: Promise<T>;
+    onSuccess?: (response: T) => IterableIterator<any>;
+    onFailure?: (error: E) => IterableIterator<any>;
 }
 
-export function* fetchSaga() {
-    yield takeEvery("fetchSaga", fetchWorker);
+export interface FetchAction<T, M, E = Error> {
+    type: string;
+    payload: FetchPayload<T, E>;
+    meta: M;
+}
+
+function* fetchWorker<T, M>(
+    actionPrefix: string,
+    {
+        type,
+        payload: { onFailure, onSuccess, request },
+        meta
+    }: FetchAction<T, M>
+) {
+    const prefix = type.replace(actionPrefix, "");
+    try {
+        yield put({
+            type: `${prefix}_REQUEST`
+        });
+        const response = yield call(() => request);
+        yield put({
+            type: `${prefix}_SUCCESS`,
+            payload: response,
+            meta
+        });
+        if (onSuccess) {
+            yield call(onSuccess, response);
+        }
+    } catch (error) {
+        yield put({
+            type: `${prefix}_FAILED`,
+            payload: error,
+            meta,
+            error: true
+        });
+        if (onFailure) {
+            yield call(onFailure, error);
+        }
+    }
+}
+
+export function* fetchSaga<T, M>(actionPrefix = "FETCH_") {
+    yield takeEvery(
+        ({ payload, type }: FetchAction<T, M>) =>
+            type && type.startsWith(actionPrefix) && payload.request != null,
+        function*(action: FetchAction<T, M>) {
+            yield call(fetchWorker, actionPrefix, action);
+        }
+    );
 }
